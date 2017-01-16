6b29d7d0b762700e36de92dfbfcdde502c4f947d
Linus <linus@dyn1226-210.wlan.ic.ac.uk>
2017-01-16 15:41:40 +0000

    updating testStrains

--- woidModel/testStrains.m
+++ woidModel/testStrains.m
@@ -4,20 +4,18 @@
 clear
 
 N = 40;
-M = 18;
+M = 50;
 L = 8/2;
-dT = 1.2/17/0.33/4;
+dT = 1.2/17/0.33/4/2;
 T = 10000;
 
-xyphiarray = runWoids(T,N,M,L,'bc','noflux','dT',dT,...
-    'theta_0',pi*40/180);
+xyphiarray = runWoids(T,N,M,L,'bc','noflux','dT',dT);
 animateWoidTrajectories(xyphiarray,'tests/DA609_noflux');
 
 xyphiarray = runWoids(T,N,M,L,'bc','noflux','dT',dT,...
     'v0',0.14,...
     'omega_m',2*pi*0.26,...
     'theta_0',pi*37/180,...
-    'deltaPhase',2*pi/M*1.2/0.68,...
     'revRate',1/11,...
     'revTime',1.5);
 animateWoidTrajectories(xyphiarray,'tests/N2_noflux');
