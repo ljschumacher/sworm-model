37a831cf23646b56f55879425dbdb6cccd2e8dd0
Linus <ljschumacher@users.noreply.github.com>
2017-05-31 17:46:45 +0100

    phase portrait with slowing down

--- woidModel/runWoidPhasePortrait.m
+++ woidModel/runWoidPhasePortrait.m
@@ -17,15 +17,16 @@ rc = 0.035;
 paramAll.bc = 'periodic'; % bc: boundary condition, 'free', 'periodic', or 'noflux' (default 'free'), can be single number or 2 element array {'bcx','bcy'} for different bcs along different dimensions
 % -- slow-down parameters --
 paramAll.vs = 0;% vs: speed when slowed down (default v0/3)
-paramAll.slowingNodes = [];% slowingNodes: which nodes register contact (default [1 M], ie head and tail)
+paramAll.slowingNodes = [1:max(round(M/10),1) (M-max(round(M/10),1)+1):M];% slowingNodes: which nodes register contact (default head and tail)
 % -- Lennard-Jones parameters --
 paramAll.r_LJcutoff = 5*rc;% r_LJcutoff: cut-off above which LJ-force is not acting anymore (default 0)
 paramAll.sigma_LJ = 2*rc;  % particle size for Lennard-Jones force
 
 revRatesClusterEdge = [0, 0.1, 0.2, 0.4, 0.8];
-speeds = [0.14, 0.33];
-attractionStrengths = [0, 1e-5, 5e-5, 1e-4];
-paramCombis = combvec(revRatesClusterEdge,speeds,attractionStrengths);
+speeds = [0.33];
+slowspeeds = [0.33, 0.2, 0.1, 0.05];
+attractionStrengths = [0];
+paramCombis = combvec(revRatesClusterEdge,speeds,slowspeeds,attractionStrengths);
 nParamCombis = size(paramCombis,2);
 parfor paramCtr = 1:nParamCombis
     param = paramAll;
@@ -36,10 +37,12 @@ parfor paramCtr = 1:nParamCombis
     param.v0 = speed;
     param.dT = min(1/2,rc/param.v0/8); % dT: time step, scales other parameters such as velocities and rates
     saveevery = round(1/4/param.dT);
-    attractionStrength = paramCombis(3,paramCtr);
+    param.vs = paramCombis(3,paramCtr);
+    attractionStrength = paramCombis(4,paramCtr);
     param.eps_LJ = attractionStrength;
-    filename = ['woids_v0_' num2str(param.v0,'%1.0e') '_epsLJ_'...
-        num2str(attractionStrength,'%1.0e')...
+    filename = ['woids_v0_' num2str(param.v0,'%1.0e') 
+        '_vs_' num2str(param.vs,'%1.0e') ...
+        '_epsLJ_' num2str(attractionStrength,'%1.0e') ...
         '_revRateClusterEdge_' num2str(param.revRateClusterEdge,'%1.0e')];
     if ~exist(['results/woids/' filename '.mat'],'file')
         xyarray = runWoids(T,N,M,L,param);
