0285da458044ed22f56af39ba832112187ad6cb4
Linus <ljschumacher@users.noreply.github.com>
2017-06-06 15:27:39 +0100

    testing slowing down proportional to the amount of contact

--- woidModel/countWoidNeighbors.m
+++ woidModel/countWoidNeighbors.m
@@ -1,17 +1,17 @@
-function [ nbrNums ] = countWoidNeighbors(distanceMatrix,r,nodeIndcs)
-% computes number of neighbours for each object at r_ij<r
+function [ numNodeswNbrs ] = countWoidNeighbors(distanceMatrix,r,nodeIndcs)
+% counts how many nodes of each object have neighbours at r_ij<r
 % distance matrix should have the form N by M by N by M with scalar
 % distance values
 if nargin<3
     nodeIndcs = 1:size(distanceMatrix,2);
 end
 N = size(distanceMatrix,1);
-% check distance from each node (of each object) to any other nodes of all other objects
+% check distance from each specified node (of each object) to any other nodes of all other objects
 nbrNMNLogInd = squeeze(any(distanceMatrix(:,nodeIndcs,:,:)<=r,4));
 for n = 1:N
     nbrNMNLogInd(n,:,n) = false; % exclude self from neighbors
 end
-nbrNMLogInd = any(nbrNMNLogInd(n,:,n),3); 
-nbrNums = sum(nbrNMLogInd,2); % count how many nodes of each object have neighbours
+nbrNMLogInd = any(nbrNMNLogInd,3); 
+numNodeswNbrs = sum(nbrNMLogInd,2); % count how many nodes of each object have neighbours
 end
 
--- woidModel/slowWorms.m
+++ woidModel/slowWorms.m
@@ -1,7 +1,14 @@
 function [ v, omega ] = slowWorms(distanceMatrix,ri,slowingNodes,vs,v0,omega_m)
 % slow down worms based on contact with other worms
-slowLogInd = findWoidNeighbors(distanceMatrix,ri,slowingNodes);
-v = vs*slowLogInd + v0*(~slowLogInd); % adjust speed for slowed worms
-omega = omega_m*(~slowLogInd + vs/v0*slowLogInd); % adjust internal oscillator freq for slowed worms
+% slowLogInd = findWoidNeighbors(distanceMatrix,ri,slowingNodes);
+% v = vs*slowLogInd + v0*(~slowLogInd); % adjust speed for slowed worms
+% omega = omega_m*(~slowLogInd + vs/v0*slowLogInd); % adjust internal oscillator freq for slowed worms
+
+% slow worms proportional to how many nodes are in contact with other nodes
+m_nbr_max = numel(slowingNodes);
+m_nbr = countWoidNeighbors(distanceMatrix,ri,slowingNodes); % number of nodes in contact with neighbours
+v = v0*(1 - m_nbr/m_nbr_max) + vs*(m_nbr/m_nbr_max);
+assert(~any(v>v0))
+omega = omega_m*v/v0; % adjust internal oscillator freq for slowed worms
 end
 
