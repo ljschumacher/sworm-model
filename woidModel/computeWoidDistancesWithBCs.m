f85c21c6e4b20df8b30f486ee7af540d2dfb2469
ljschumacher <lschum@me.com>
2017-06-07 16:00:22 +0100

    further performance improvments to computeWoidDistancesWithBCs_mex, ca. 30%

--- woidModel/computeWoidDistancesWithBCs.m
+++ woidModel/computeWoidDistancesWithBCs.m
@@ -10,8 +10,7 @@ M = size(positionArray,2);
 distanceMatrix = NaN(N*M,N*M,ndim);
 positionArrayStacked = reshape(positionArray,N*M,ndim);
 for dimCtr = 1:ndim
-    distanceMatrix(:,:,dimCtr) = positionArrayStacked(:,dimCtr*ones(1,N*M))...
-        - positionArrayStacked(:,dimCtr*ones(1,N*M))';
+    distanceMatrix(:,:,dimCtr) = bsxfun(@minus,positionArrayStacked(:,dimCtr),positionArrayStacked(:,dimCtr)');
     % reset some distances if boundaries are periodic
     if (~iscell(bc)&&strcmp(bc,'periodic'))||(iscell(bc)&&strcmp(bc{dimCtr},'periodic'))
         if numel(L)==ndim % vector domain size [L_x L_y] ie non-square domain
