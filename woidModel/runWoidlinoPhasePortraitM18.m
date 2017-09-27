76adde47ee5ca21a6464c09c2101bdde501ab3af
Linus <ljschumacher@users.noreply.github.com>
2017-09-27 16:34:14 +0100

    missing files from last commit and visualisation of LJ force around model schematic

---
+++ woidModel/runWoidlinoPhasePortraitM18.m
@@ -0,0 +1,82 @@
+% run simulations of simplified woid model with single node per woid
+% for various speeds, attractions strengths, reversal probabilities...
+
+% issues/todo:
+
+clear
+close all
+
+% general model parameters for all test - unless set otherwise
+N = 40; % N: number of objects
+M = 18; % M: number of nodes in each object
+L = [7.5, 7.5]; % L: size of region containing initial positions - scalar will give circle of radius L, [Lx Ly] will give rectangular domain
+numRepeats = 10;
+
+T = 500;
+rc0 = 0.035; % rc: core repulsion radius (default 0.035 mm)
+paramAll.rc = 0;
+paramAll.ri = 3*rc0;
+% saveevery = round(1/2/param.dT);
+paramAll.bc = 'periodic'; % bc: boundary condition, 'free', 'periodic', or 'noflux' (default 'free'), can be single number or 2 element array {'bcx','bcy'} for different bcs along different dimensions
+paramAll.segmentLength = 1.13/(M - 1);
+paramAll.k_l = 40; % stiffness of linear springs connecting nodes
+% -- reversal parameters --
+paramAll.revRate = 0;
+paramAll.revRateCluster = 0;
+% -- slow-down parameters --
+paramAll.vs = 0;% vs: speed when slowed down (default v0/3)
+paramAll.slowingNodes = 1:M;% slowingNodes: which nodes register contact (default head and tail)
+paramAll.headNodes = 1:max(round(M/10),1);
+paramAll.tailNodes = (M-max(round(M/10),1)+1):M;
+% -- Lennard-Jones parameters --
+paramAll.r_LJcutoff = 5*rc0;% r_LJcutoff: cut-off above which LJ-force is not acting anymore (default 0)
+paramAll.sigma_LJ = 2*rc0;  % particle size for Lennard-Jones force
+% -- undulation parameters --
+paramAll.theta_0 = 0;
+paramAll.omega_m = 0;
+paramAll.deltaPhase = 0;
+
+revRatesClusterEdge = [0, 0.1, 0.2, 0.4, 0.8, 1.6];
+speeds = [0.33];
+slowspeeds = fliplr([0.33, 0.1, 0.05, 0.025, 0.0125]);
+attractionStrengths = [0, 2e-3];
+paramCombis = combvec(revRatesClusterEdge,speeds,slowspeeds,attractionStrengths);
+nParamCombis = size(paramCombis,2);
+for repCtr = 1:numRepeats
+    for paramCtr = 1:nParamCombis
+        param = paramAll;
+        revRateClusterEdge = paramCombis(1,paramCtr);
+        param.revRateClusterEdge = revRateClusterEdge;
+        speed = paramCombis(2,paramCtr);
+        param.v0 = speed;
+        param.dT = min(1/2,rc0/param.v0/16); % dT: time step, scales other parameters such as velocities and rates
+        param.saveEvery = round(1/4/param.dT);
+        param.vs = paramCombis(3,paramCtr);
+        attractionStrength = paramCombis(4,paramCtr);
+        if attractionStrength>0
+            param.r_LJcutoff = 5*rc0;
+        else
+            param.r_LJcutoff = -1; % don't need to compute attraction if it's zero
+        end
+        param.eps_LJ = attractionStrength;
+        filename = ['wlM' num2str(M) '_N_' num2str(N) '_L_' num2str(L(1)) '_noVolExcl'...
+            '_v0_' num2str(param.v0,'%1.0e') '_vs_' num2str(param.vs,'%1.0e') ...
+            '_gradualSlowDown' ...
+            '_epsLJ_' num2str(attractionStrength,'%1.0e') ...
+            '_revRateClusterEdge_' num2str(param.revRateClusterEdge,'%1.0e')...
+            '_run' num2str(repCtr)];
+        if ~exist(['results/woidlinos/' filename '.mat'],'file')&&isempty(dir(['results/woidlinos/' filename '_running_on_*.mat']))
+            disp(['running ' filename])
+            % make a dummy file to mark that this sim is running on this computer
+            [~, hostname] = system('hostname -s'); hostname = strrep(hostname,sprintf('\n'),'');
+            tmp_filename = ['results/woidlinos/' filename '_running_on_' hostname '.mat'];
+            save(tmp_filename,'N','M','L','param')
+            rng(repCtr) % set random seed to be the same for each simulation
+            xyarray = runWoids(T,N,M,L,param);
+            xyarray = single(xyarray); % save space by using single precision
+            saveResults(['results/woidlinos/' filename '.mat'],...
+                struct('xyarray',xyarray,'T',T,'N',N,'M',M,'L',L,'param',param))
+            delete(tmp_filename)
+        end
+    end
+end
---
+++ woidModel/visualisation/drawWoidSchematicWithLJforce.m
@@ -0,0 +1,75 @@
+function [ ] = drawWoidSchematicWithLJforce( )
+close all
+addpath('../')
+% draws a schematic of the agent based model
+N = 1;
+M = 49;
+rc = 0.035;
+L = 1.25;
+segmentLength = 1.13/(M-1);
+deltaPhase = 0.25;
+theta_0 = pi/4;
+bc = 'free';
+angles = linspace(0,2*pi,48)'; % for plotting node size
+
+phaseOffset = wrapTo2Pi(deltaPhase*(1:M));
+rng(1)
+[xyarray, ~] = initialiseWoids(N,M,1,L,segmentLength,phaseOffset,theta_0,rc,bc);
+
+% plot lennard jones force
+eps_LJ = 1;
+r_LJcutoff = 5*rc;
+sigma_LJ = 2*rc;
+f_LJ = @(x) 48*eps_LJ./x.*((sigma_LJ./x).^6 - 1/2*(sigma_LJ./x).^3);
+
+x = linspace(-0.1,1.2,2600);
+y = linspace(-0.2,0.3,1000);
+[X,Y] = meshgrid(x,y);
+F_LJ = zeros(size(X));
+for nodeCtr = 1:M
+r = sqrt((x - xyarray(:,nodeCtr,1)).^2 + (y - xyarray(:,nodeCtr,2))'.^2);
+% ignore values smaller than the node radius for plotting
+r(r<2*rc) = 2*rc;
+r(r>r_LJcutoff) = Inf;
+F_LJ = F_LJ + f_LJ(r);
+end
+F_LJ(F_LJ>0) = -min(min(F_LJ));
+h = pcolor(X,Y,F_LJ);
+h.MeshStyle = 'none';
+hold on
+% load divergent colormap
+divcmap = load('/Users/linus/Dropbox/Utilities/colormaps_ascii/diverging/cmap_BuRd.txt');
+colormap(divcmap)
+
+% plot worm
+patch(xyarray(:,:,1,1) + rc*cos(angles),...
+            xyarray(:,:,2,1) + rc*sin(angles),...
+            0.5*ones(1,3),'EdgeColor','None')
+plot(xyarray(:,:,1),xyarray(:,:,2),'-',...
+    'Marker','.','Color','k','MarkerSize',12);
+plot(xyarray(:,1,1),xyarray(:,1,2),'-',...
+    'Marker','.','Color','r','MarkerSize',12);
+ax = gca;
+ax.Color = 'none';
+ax.DataAspectRatio = [1 1 1];
+ax.Visible = 'on';
+ax.XLabel.String = 'x';
+ax.YLabel.String = 'y';
+
+hcb = colorbar;
+hcb.Label.String = 'Lennard-Jones Force';
+
+exportOptions = struct('Format','eps2',...
+    'Color','rgb',...
+    'Width',14,...
+    'Resolution',300,...
+    'LineWidth',1);
+
+set(gcf,'PaperUnits','centimeters')
+filename = ['modelSchematicWithLJForce'];
+exportfig(gcf,[filename '.eps'],exportOptions);
+system(['epstopdf ' filename '.eps']);
+system(['rm ' filename '.eps']);
+
+end
+
