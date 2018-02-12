0e3c96bd8767d4e283d7892dc458e27ac9d75a57
Linus Schumacher <schumacher@physics.org>
2018-02-12 20:06:22 +0000

    log samples N@

--- woidModel/extendSimulation.m
+++ woidModel/extendSimulation.m
@@ -3,7 +3,7 @@ function [] = extendSimulation(filename,Textend)
 load(filename)
 [xyarray2, currentState] = runWoids(Textend,N,M,L,param,'resumeState',currentState);
 xyarray = cat(4,xyarray,xyarray2(:,:,:,2:end));
-filename = [filename '_extended'];
+filename = [strrep(filename,'.mat',''),'_extended.mat'];
 T = T + Textend;
 clear xyarray2
 save(filename)
