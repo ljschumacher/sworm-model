1c9de12ee3af7018b321b3bb781431753b290f31
Linus Schumacher <schumacher@physics.org>
2018-02-18 15:03:38 +0000

    made a note about a future change of implementation in updating the dwelling state

--- woidModel/updateDwellingState.m
+++ woidModel/updateDwellingState.m
@@ -12,6 +12,9 @@ end
 
 % dwelling worms stop dwelling with rate k_undwell, decreasing
 %   exponentially with more neighbours
+% NOTE: thsi current implentation allows worms that just stopped to speed
+% up again. you may not want this, but it's currently kept for backwards
+% compatibility (consistency with previous results) 
 if any(dwellLogInd)
     dwellers = find(dwellLogInd);
     undwellrate = k_undwell*exp(-dkdN_undwell.*num_nbr_per_node);
