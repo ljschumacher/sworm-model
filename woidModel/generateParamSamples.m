2e19e4a92f937ef27c729b271f2ee552fa5822b2
Linus Schumacher <schumacher@physics.org>
2018-08-13 18:01:33 +0100

    param samples and script for running 4D inference samples

--- woidModel/generateParamSamples.m
+++ woidModel/generateParamSamples.m
@@ -2,7 +2,7 @@
 clear all
 
 nSamples = 1e5; % number of samples
-nParam = 3; % number of parameters
+nParam = 4; % number of parameters
 
 % seed random number generator for reproducibility
 rng(1)
@@ -15,11 +15,13 @@ slowingMode = 'stochastic_bynode';
 k_dwell = 0.0036;
 k_undwell = 1.1;
 reversalMode = 'density';
+haptotaxisMode = 'weighted_additive';
+
 % load reduced prior gmmodel
-load(['priors3D_M_' num2str(M) '_noVolExcl' ...
+load(['priors4D_M_' num2str(M) '_noVolExcl' ...
     '_angleNoise_' num2str(angleNoise) '_k_theta_' num2str(k_theta)...
     '_slowing_' slowingMode '_dwell_' num2str(k_dwell) '_' num2str(k_undwell)...
-    '_rev' reversalMode ...'_haptotaxis_' num2str(f_hapt) ...
+    '_rev' reversalMode '_haptotaxis_' haptotaxisMode ...
     '.mat'],'supportLimits','prior_npr1')
 
 % sample from prior
@@ -40,13 +42,15 @@ dkdN_dwell = samplesRaw(:,2);
 
 dkdN_undwell = samplesRaw(:,3);
 
+f_hapt = samplesRaw(:,4);
+
 % make a table of the parameters
-paramSamples = table(drdN_rev,dkdN_dwell,dkdN_undwell);
+paramSamples = table(drdN_rev,dkdN_dwell,dkdN_undwell,f_hapt);
 
 % save parameter samples
 save(['paramSamples_nSamples' num2str(nSamples) '_nParam' num2str(nParam) ...
     '_M_' num2str(M) '_noVolExcl' ...
     '_angleNoise_' num2str(angleNoise) '_k_theta_' num2str(k_theta)...
     '_slowing_' slowingMode '_dwell_' num2str(k_dwell) '_' num2str(k_undwell)...
-    '_rev' reversalMode ...'_haptotaxis_' num2str(f_hapt)
+    '_rev' reversalMode '_haptotaxis_' haptotaxisMode ...
     '.mat'],'paramSamples','supportLimits')
--- woidModel/runWoidlinoParamSamples.m
+++ woidModel/runWoidlinoParamSamples.m
@@ -32,31 +32,34 @@ param.omega_m = 0;
 param.deltaPhase = 0;
 param.angleNoise = 0.05;
 % -- haptotaxis --
-% param.f_hapt = 0.5;
+param.Rif = 1.2/0.035;
+param.haptotaxisMode = 'weighted_additive';
 % -- speed and time-step --
 param.v0 = [0.33]; % npr1 0.33; N2 0.14
 param.dT = min(1/2,rc/param.v0/8); % dT: time step, scales other parameters such as velocities and rates
 param.saveEvery = round(1/param.dT);
 
 % load parameter samples
-load(['paramSamples_nSamples100000_nParam3_M_' num2str(M) '_noVolExcl' ...
+load(['paramSamples_nSamples100000_nParam4_M_' num2str(M) '_noVolExcl' ...
     '_angleNoise_' num2str(param.angleNoise) '_k_theta_' num2str(param.k_theta)...
     '_slowing_' param.slowingMode '_dwell_' num2str(param.k_dwell) '_' num2str(param.k_undwell)...
-    '_rev' param.reversalMode ...'_haptotaxis_' num2str(f_hapt)
+    '_rev' param.reversalMode '_haptotaxis_' num2str(param.haptotaxisMode)...
     '.mat'],'paramSamples','supportLimits')
 % set model parameters from generated samples
 param.drdN_rev = paramSamples.drdN_rev(sampleCtr);
 param.dkdN_dwell = paramSamples.dkdN_dwell(sampleCtr);
 param.dkdN_undwell = paramSamples.dkdN_undwell(sampleCtr);
+param.f_hapt = paramSamples.f_hapt(sampleCtr);
 
-% filepath = '/exports/eddie/scratch/lschuma2/woidlinos/';
-filepath = 'results/woidlinos/paramSamples/PRW_3D_r1/';
+filepath = '/exports/eddie/scratch/lschuma2/woidlinos/PRW_4D_r1/';
+% filepath = 'results/woidlinos/paramSamples/PRW_4D_r1/';
 filename = ['wlM' num2str(M) '_N_' num2str(N) '_L_' num2str(L(1)) ...
     '_v0_' num2str(param.v0) '_vs_' num2str(param.vs) ...
     '_angleNoise_' num2str(param.angleNoise) '_k_theta_' num2str(param.k_theta)...
     '_slow_' param.slowingMode '_dwell_' num2str(param.k_dwell) '_' num2str(param.k_undwell)...
     '_dkdN_' num2str(param.dkdN_dwell) '_' num2str(param.dkdN_undwell)...
     '_rev' param.reversalMode '_drdN_' num2str(param.drdN_rev) ...
+    '_haptotaxis_' param.haptotaxisMode '_' num2str(param.f_hapt) ...
     '_sample_' num2str(sampleCtr)];
 if ~exist([filepath filename '.mat'],'file')
     %% check for pair stability, which we don't want
@@ -93,7 +96,7 @@ if ~exist([filepath filename '.mat'],'file')
             disp(['params result in unstable cluster (Rgyr ' num2str(Rgyr) ...
                 '), discontinuing simulation'])
             % save Rgyr result?
-        elseif Rgyr<=4
+        elseif Rgyr<=3
             %% run full-length simulation
             disp('Running full-length simulation...')
             param.bc = 'periodic';
